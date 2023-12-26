138f37e3371ed1310362c9ab0ea59f3f5d59a4de
Rohan Julka <rohanjulka19@gmail.com>
2023-12-26 19:34:39 +0530

    setup context

--- index.ts
+++ index.ts
@@ -1,11 +1,17 @@
-function getCanvasElement(): HTMLCanvasElement {
-    const canvas = document.getElementById('canvas') 
+function getCanvasElement(elementId: string): HTMLCanvasElement {
+    const canvas = document.getElementById(elementId) 
     if (!(canvas instanceof HTMLCanvasElement)) {
         throw new Error("Unable to find canvas element");
     }
     return canvas
 }
 
-const canvas = getCanvasElement()
-const ctx = canvas.getContext('2d');
+function getCanvasContext(canvas: HTMLCanvasElement): CanvasRenderingContext2D {
+    const ctx = canvas.getContext('2d')
+    if (!ctx) {
+        throw new Error("Browser does not support 2d Context")
+    }
+    return ctx
+}
 
+const ctx = getCanvasContext(getCanvasElement('canvas'))
